1591bff81781f64ea177fd07a94c5e7d887e4e00
Caleb Foster <foster.caleb@gmail.com>
2017-11-09 17:00:00 -0600

    implement Iterator

--- src/Collections/LinkedList.php
+++ src/Collections/LinkedList.php
@@ -9,7 +9,7 @@ use calebfoster\Collections\LinkedList\Node;
 *
 *  @author Caleb Foster
 */
-class LinkedList
+class LinkedList implements \Iterator
 {
 
     /** @var Node */
@@ -20,6 +20,9 @@ class LinkedList
 
     /** @var int */
     protected $count;
+
+    /** @var Node */
+    private $current;
 
     public function __construct($root = null)
     {
@@ -71,7 +74,7 @@ class LinkedList
     {
         $node = $this->toNode($node);
 
-        if (!$this->count) {
+        if ($this->count === 0) {
             $this->head = $node;
         }
         else {
@@ -82,5 +85,96 @@ class LinkedList
         $this->count++;
 
         return $this;
+    }
+
+    public function removeFirst()
+    {
+        if ($this->count !== 0) {
+            $this->head = $this->head->next;
+            $this->count--;
+
+            if ($this->count === 0)
+                $this->tail = null;
+        }
+    }
+
+    /**
+     * @return LinkedList
+     */
+    public function removeLast()
+    {
+        if ($this->count !== 0) {
+            if ($this->count === 1) {
+                $this->head = $this->tail = null;
+            }
+            else {
+                $current = $this->head;
+                while ($current->next !== $this->tail)
+                    $current = $current->next;
+
+                $current->next = null;
+                $this->tail = $current;
+            }
+
+            $this->count--;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return $this->current;
+    }
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        $this->current = $this->current->next;
+    }
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        // TODO: Implement key() method.
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return !!$this->current;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        $this->current = $this->head;
     }
 }
